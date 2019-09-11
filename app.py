4dc09914959679e95bb40bf488fb1c668513943b
alexbarksdale <ale.barksdale@gmail.com>
2019-09-10 17:02:48 -0700

    testing merge

--- app.py
+++ app.py
@@ -4,20 +4,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/joke')
+@app.route('/')
 def make_joke():
-    params = { "gifs": 'faces',
-    "Key": "1F2TY5LFTDOH" }
+    params = {"gifs": 'faces',
+              "Key": "1F2TY5LFTDOH"}
     response = requests.get(
-    'https://api.tenor.com/v1/search',
-    params=params)
-
+        'https://api.tenor.com/v1/search',
+        params=params)
 
     joke_json = response.json()
     joke_str = joke_json
     return joke_str
 
 
-
-if __name__ =="__main__":
-    app.run(debug=True,port=8080)
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)

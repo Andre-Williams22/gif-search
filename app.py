89e05299fd90dfd0c252af7ee945d9b4988f8611
Andre <awjrs22@gmail.com>
2019-09-12 12:40:31 -0700

    loads all json data from search term button

--- app.py
+++ app.py
@@ -16,17 +16,26 @@ def index():
 
 @app.route('/gif', methods=["GET"])
 def make_gif():
+ 
+    search_term = request.args.get('search-term')
 
-    response = requests.get(f'https://api.tenor.com/v1/search?q={search-term}&key=1F2TY5LFTDOH&limit=10')
-       
-    images = json.loads(response.text)['data']
+    response = requests.get(f'https://api.tenor.com/v1/search?q={search_term}&key=1F2TY5LFTDOH&limit=10')
 
-   # urls = [image['images']['fixed_width']['url'] for image in images
+    if response.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        top_10gifs = json.loads(response.content)
+        return top_10gifs
+    else:
+        top_10gifs = None
+    '''
+    images = json.loads(response.text)
 
-    return render_template('gif.html', images=images)
+    urls = [image['images']['fixed_width']['url'] for image in images
+
+    return render_template('gif.html', response=response)
     #images = json.loads(response.text)['data']
     # creates a list of urls and thier fixed images
-    #urls = [image['images']['fixed_width']['url'] for image in images]
+    #urls = [image['images']['fixed_width']['url'] for image in images]'''
 
 
 

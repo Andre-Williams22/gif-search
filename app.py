cb98410d551a70987ea1f0bd2c4682d294f26130
Andre <awjrs22@gmail.com>
2019-09-12 12:16:59 -0700

    renders images

--- app.py
+++ app.py
@@ -16,19 +16,17 @@ def index():
 
 @app.route('/gif', methods=["GET"])
 def make_gif():
-    params = {
-    "q": 'fun',
-    "Key": "1F2TY5LFTDOH"
-}
-    response = requests.get(
-        'https://api.tenor.com/v1/search',
-        params=params)
-  
+
+    response = requests.get(f'https://api.tenor.com/v1/search?q={search-term}&key=1F2TY5LFTDOH&limit=10')
+       
+    images = json.loads(response.text)['data']
+
+   # urls = [image['images']['fixed_width']['url'] for image in images
+
+    return render_template('gif.html', images=images)
     #images = json.loads(response.text)['data']
     # creates a list of urls and thier fixed images
     #urls = [image['images']['fixed_width']['url'] for image in images]
-    
-    return render_template('gif.html', response=response)
 
 
 

913805a0029c6089a25edbfeccfce8df8a8677e8
Andre <awjrs22@gmail.com>
2019-09-12 11:53:51 -0700

    tenor api

--- app.py
+++ app.py
@@ -16,15 +16,17 @@ def index():
 
 @app.route('/gif', methods=["GET"])
 def make_gif():
-    # data=json.loads(s.urlopen("http://api.giphy.com/v1/gifs/search?q=nerdy&api_key=RszCWrqNToHdZUk9E2o7f2TPk5Srv6y4&q&limit=40&offset=0&rating=G&lang=en").read())
-    # return json.dumps(data, sort_keys=True, indent=4)
-    search_term = request.args.get('search-term')
-
-    response = requests.get(f'https://api.giphy.com/v1/gifs/search?api_key=RszCWrqNToHdZUk9E2o7f2TPk5Srv6y4&q={search_term}&limit=25&offset=0&rating=G&lang=en')
-    
-    images = json.loads(response.text)['data']
+    params = {
+    "q": 'fun',
+    "Key": "1F2TY5LFTDOH"
+}
+    response = requests.get(
+        'https://api.tenor.com/v1/search',
+        params=params)
+  
+    #images = json.loads(response.text)['data']
     # creates a list of urls and thier fixed images
-    urls = [image['images']['fixed_width']['url'] for image in images]
+    #urls = [image['images']['fixed_width']['url'] for image in images]
     
     return render_template('gif.html', urls=urls)
 

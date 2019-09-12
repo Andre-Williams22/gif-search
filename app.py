4364cd9e5766a412f60cd8184dc86d8fb2b48665
Andre <awjrs22@gmail.com>
2019-09-11 19:03:50 -0700

    adds the giphy api for pulling gifs

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 
 from flask import Flask, render_template, request, url_for
-import giphypop
+import requests
 import os
-
+import urllib.request as ur,json
+import json
 
 app = Flask(__name__)
 
 
 # define a function that searches the Giphypop API with the giphypop library
-def get_search_function(search_term):
-    g = giphypop.Giphy()  # giphypop api
-    results = g.search(search_term)
-    return results
+
 
 # represents home page
 @app.route('/')
@@ -19,21 +17,21 @@ def index():
     return render_template('index.html')  # points to index.html page
 
 
-@app.route('/results')
-def results():
-    gif_search = request.values.get('gif_search')
+@app.route('/gif', methods=["GET"])
+def make_gif():
+    # data=json.loads(s.urlopen("http://api.giphy.com/v1/gifs/search?q=nerdy&api_key=RszCWrqNToHdZUk9E2o7f2TPk5Srv6y4&q&limit=40&offset=0&rating=G&lang=en").read())
+    # return json.dumps(data, sort_keys=True, indent=4)
+    search_term = request.args.get('search-term')
 
-    if gif_search == "":
-        gif_list = []
-    else:
-        gif_list = get_search_function(gif_search)
-# points to the result web-page and send requested information to this page so that it is customized
-    return render_template('results.html', gif_search=gif_search, gif_list=gif_list)
+    response = requests.get(f'https://api.giphy.com/v1/gifs/search?api_key=RszCWrqNToHdZUk9E2o7f2TPk5Srv6y4&q={search_term}&limit=25&offset=0&rating=G&lang=en')
+    # helped by IKE and Medi
+    images = json.loads(response.text)['data']
+    urls = [image['images']['fixed_width']['url'] for image in images]
+    
+    return render_template('gif.html', urls=urls)
 
 
-@app.route('/search')
-def search_form():
-    return render_template('index.html')
+
 
 
 if __name__ == "__main__":
